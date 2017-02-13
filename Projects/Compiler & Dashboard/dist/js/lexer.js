a5098e64e6043e3fc714b0b0a826eff0cd39b272
Tienza <Piradon.Liengtiraphan1@gmail.com>
2017-02-13 16:12:43 -0500

    Reorganized functions and variables to fix appropriate scopes

--- Projects/Compiler & Dashboard/dist/js/lexer.js
+++ Projects/Compiler & Dashboard/dist/js/lexer.js
@@ -1,77 +1,9 @@
-var verbose = true;
-
-var byChar = true;
-
-var $textarea = $('#log');
-
 // Creates the Token Class to store token information 
 class Token {
     constructor(tokenType, tokenValue, tokenLine) {
         this.type = tokenType;
         this.value = tokenValue;
         this.line = tokenLine;
-    }
-}
-
-// Function to do a "replace all" on a string. Because this doesn't exit natively in JavaScript
-String.prototype.replaceAll = function(search, replacement) {
-    var target = this;
-    return target.split(search).join(replacement);
-};
-
-// Function to do a "replace all" on a string using ReGex. Because this doesn't exit natively in JavaScript
-String.prototype.replaceAllRX = function(search, replacement) {
-    var target = this;
-    return target.replace(new RegExp(search, 'g'), replacement);
-};
-
-// Function to do delete a value from an array. Because apparently this doesn't exit natively in JavaScript
-Array.prototype.clean = function(deleteValue) {
-    for (var i = 0; i < this.length; i++) {
-        if (this[i] == deleteValue) {
-            this.splice(i, 1);
-            i--;
-        }
-    }
-    return this;
-};
-
-// Function that returns a boolean if there is a regex match, used during lex
-function isMatch(rx, str) {
-    if (str.match(rx))
-        return true;
-    else
-        return false;
-}
-
-// Toggles whether the compiler will be in "Verbose Mode" or not. (Might move to different file later)
-function toggleVerbose() {
-    verbose = !verbose;
-
-    if (!verbose) {
-        $('#verboseToggle').html("Verbose: Off")
-        $('#verboseToggle').toggleClass('btn btn-danger btn-lg');
-        $('#verboseToggle').toggleClass('btn btn-default btn-lg');
-		$('#verboseDropDown').toggleClass('btn btn-danger btn-lg dropdown-toggle');
-        $('#verboseDropDown').toggleClass('btn btn-default btn-lg dropdown-toggle');
-    } else {
-        $('#verboseToggle').html("Verbose: On")
-        $('#verboseToggle').toggleClass('btn btn-default btn-lg');
-        $('#verboseToggle').toggleClass('btn btn-danger btn-lg');
-        $('#verboseDropDown').toggleClass('btn btn-default btn-lg dropdown-toggle');
-		$('#verboseDropDown').toggleClass('btn btn-danger btn-lg dropdown-toggle');
-		
-    }
-}
-
-// Toggles whether the Lexer will treat strings as String Blocks or CharLists
-function toggleByChar() {
-	byChar = !byChar;
-	
-	if (!byChar) {
-        $('#byCharToggle').html("Lex String By: Char")
-    } else {
-        $('#byCharToggle').html("Lex String By: Block")
     }
 }
 
@@ -111,9 +43,9 @@ function lex() {
     // Replaces breaklines with spaces and turns whole input into single lined string
     //str = str.replace(/(\r\n|\n|\r)/gm, " ");
     if (/\S/.test(str)) {
-        // ReGex pattern to break up input by symbols, keywords, etc.
-		// Must also give credit where it is due, this ReGex a modified form of the ReGex from Svegliator which is a modified
-		// version of the ReGex from apparent ReGex God "Chris"
+        // RegEx pattern to break up input by symbols, keywords, etc.
+		// Must also give credit where it is due, this RegEx a modified form of the RegEx from Svegliator which is a modified
+		// version of the RegEx from apparent RegEx God "Chris"
         DELIMITER_PATTERN = /([a-z]+)|(\d+)|("[^"]*")|(==)|(!=)|(\S)|(\n)/g;
 
         // Turns string into array delimited by the pattern above
@@ -143,7 +75,7 @@ function lex() {
             lexeme = codeFrag[i].trim();
 
             if (lexeme !== undefined) {
-                // Regex for Print
+                // RegEx for Print
                 if (isMatch(/^print$/, lexeme)) {
                     var token = new Token("T_PRINT", "print", lineNum);
 
@@ -154,7 +86,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for While 
+                // RegEx for While 
                 else if (isMatch(/^while$/, lexeme)) {
                     var token = new Token("T_WHILE", "while", lineNum);
 
@@ -165,7 +97,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for If
+                // RegEx for If
                 else if (isMatch(/^if$/, lexeme)) {
                     var token = new Token("T_IF", "if", lineNum);
 
@@ -176,7 +108,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Variable Type
+                // RegEx for Variable Type
                 else if (isMatch(/^int$|^string$|^boolean$/, lexeme)) {
                     var type = lexeme.match(/^int$|^string$|^boolean$/);
 
@@ -189,7 +121,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Boolean Value
+                // RegEx for Boolean Value
                 else if (isMatch(/^true$|^false$/, lexeme)) {
                     var boolVal = lexeme.match(/^true|false$/);
 
@@ -202,7 +134,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Left Brace
+                // RegEx for Left Brace
                 else if (isMatch(/^{$/, lexeme)) {
                     var token = new Token("T_OPENING_BRACE", "{", lineNum);
 
@@ -213,7 +145,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Right Brace
+                // RegEx for Right Brace
                 else if (isMatch(/^}$/, lexeme)) {
                     var token = new Token("T_CLOSING_BRACE", "}", lineNum);
 
@@ -224,7 +156,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Left Parenthesis
+                // RegEx for Left Parenthesis
                 else if (isMatch(/^\($/, lexeme)) {
                     var token = new Token("T_OPENING_PARENTHESIS", "(", lineNum);
 
@@ -235,7 +167,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Right Parenthesis
+                // RegEx for Right Parenthesis
                 else if (isMatch(/^\)$/, lexeme)) {
                     var token = new Token("T_RIGHT_PARENTHESIS", ")", lineNum);
 
@@ -246,7 +178,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for End of Program Symbol
+                // RegEx for End of Program Symbol
                 else if (isMatch(/^\$$/, lexeme)) {
                     var token = new Token("T_EOPS", "$", lineNum);
 
@@ -257,7 +189,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Equality Operator
+                // RegEx for Equality Operator
                 else if (isMatch(/^==$/, lexeme)) {
                     var token = new Token("T_EQUALITY_OP", "==", lineNum);
 
@@ -268,7 +200,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Inequality Operator
+                // RegEx for Inequality Operator
                 else if (isMatch(/^!=$/, lexeme)) {
                     var token = new Token("T_INEQUALITY_OP", "!=", lineNum);
 
@@ -279,7 +211,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Assignment Operator
+                // RegEx for Assignment Operator
                 else if (isMatch(/^=$/, lexeme)) {
                     var token = new Token("T_ASSIGNMENT_OP", "=", lineNum);
 
@@ -290,7 +222,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Addition Operator
+                // RegEx for Addition Operator
                 else if (isMatch(/^\+$/, lexeme)) {
                     var token = new Token("T_ADDITION_OP", "+", lineNum);
 
@@ -301,7 +233,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Identifiers
+                // RegEx for Identifiers
                 else if (isMatch(/^[a-z]$/, lexeme)) {
                     var iden = lexeme.match(/^[a-z]$/);
 
@@ -314,7 +246,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Digits
+                // RegEx for Digits
                 else if (isMatch(/^[0-9]$/, lexeme)) {
                     var digit = lexeme.match(/^[0-9]$/);
 
@@ -327,7 +259,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for Quotation
+                // RegEx for Quotation
                 else if (isMatch(/^"$/, lexeme)) {
                     var token = new Token("T_QUOTE", "\"", lineNum);
 
@@ -338,7 +270,7 @@ function lex() {
 
                     tokens.push(token);
                 }
-                // Regex for String -- Either by String Blocks or CharLists ---
+                // RegEx for String -- Either by String Blocks or CharLists ---
                 else if (isMatch(/^(")([a-z \t]*)(")$/, lexeme)) {
                     // Creates Token for full string
                     if (!byChar) {
@@ -416,7 +348,7 @@ function lex() {
                         }
                     }
                 }
-                // Regex for White Space
+                // RegEx for White Space
                 else if (lexeme === "") {
                     if (verbose)
                         console.log(lexeme + " on line " + lineNum);
@@ -444,16 +376,16 @@ function lex() {
         var checkReturn = checkEOPS(tokens, reachedEnd, lexWarningCount, lexErrorCount);
 
         // Assigns the return values of checkEOPS
-        tokens = checkReturn[0];
-        lexWarningCount = checkReturn[1];
-		lexErrorCount = checkReturn[2];
+        tokens = checkReturn.tokenArray;
+        lexWarningCount = checkReturn.lexWarningCount;
+		lexErrorCount = checkReturn.lexErrorCount;
 
         var printLastReturn = printLastMessage(tokens, printTokens, lexWarningCount, lexErrorCount);
 
         // Assigns the return values of printLastMessage
-        tokens = printLastReturn[0];
-        lexWarningCount = printLastReturn[1];
-        lexErrorCount = printLastReturn[2];
+        tokens = printLastReturn.tokenArray;
+        lexWarningCount = printLastReturn.lexWarningCount;
+        lexErrorCount = printLastReturn.lexErrorCount;
 
         // Logs token Array
         console.log(tokens);
@@ -472,85 +404,105 @@ function lex() {
 
         printLastMessage(tokens, printTokens, lexWarningCount, lexErrorCount);
     }
-}
+	
+	function checkEOPS(tokenArray, reachedEnd, lexWarningCount, lexErrorCount) {
+		var txt = $('#log').val();
+		
+		var checkEOPS = {
+			tokenArray: "",
+			lexWarningCount: "",
+			lexErrorCount: ""
+		}
+		// Checks to see whether the program ends with a EOPS ($) or not. If not then adds EOPS to the end of the token stream
+		try {
+			if (tokenArray[tokenArray.length - 1].value != "$" && reachedEnd) {
 
-function checkEOPS(tokenArray, reachedEnd, lexWarningCount, lexErrorCount) {
-	var txt = $('#log').val();
-    // Checks to see whether the program ends with a EOPS ($) or not. If not then adds EOPS to the end of the token stream
-    try {
-		if (tokenArray[tokenArray.length - 1].value != "$" && reachedEnd) {
+				var endToken = new Token("T_EOPS", "$", tokenArray[tokenArray.length - 1].line);
+				tokenArray.push(endToken);
 
-			var endToken = new Token("T_EOPS", "$", tokenArray[tokenArray.length - 1].line);
-			tokenArray.push(endToken);
-
-			lexWarningCount++;
-			
-			if (verbose) {
-				$('#log').val(txt + " LEXER --> | WARNING! NO EOPS [$] detected. Added to end-of-file at line " + tokenArray[tokenArray.length - 1].line + "...\n");
-				$textarea.scrollTop($textarea[0].scrollHeight);
+				lexWarningCount++;
+				
+				if (verbose) {
+					$('#log').val(txt + " LEXER --> | WARNING! NO EOPS [$] detected. Added to end-of-file at line " + tokenArray[tokenArray.length - 1].line + "...\n");
+					$textarea.scrollTop($textarea[0].scrollHeight);
+				}
 			}
 		}
+		// If Token Array is invalid or empty, notify the user of the error
+		catch(error) {
+			lexErrorCount++;
+			
+			if (verbose) {
+				console.log("Invalid Input..." + error);
+				$('#log').val(txt + " LEXER --> | ERROR! Input did not generate valid Token Array...\n");
+				$textarea.scrollTop($textarea[0].scrollHeight);
+			}
+			
+		}
+
+		checkEOPS.tokenArray = tokenArray;
+		checkEOPS.lexWarningCount = lexWarningCount;
+		checkEOPS.lexErrorCount = lexErrorCount;
+
+		return checkEOPS;
 	}
-	// If Token Array is invalid or empty, notify the user of the error
-	catch(error) {
-		lexErrorCount++;
-		
-		if (verbose) {
-			console.log("Invalid Input..." + error);
-			$('#log').val(txt + " LEXER --> | ERROR! Input did not generate valid Token Array...\n");
-			$textarea.scrollTop($textarea[0].scrollHeight);
+
+	function printLastMessage(tokenArray, printTokens, lexWarningCount, lexErrorCount) {	
+		var printLastMessage = {
+			tokenArray: "",
+			lexWarningCount: "",
+			lexErrorCount: ""
 		}
 		
-	}	
+		// Decides what to print for Final Lex Message
+		if (printTokens) {
+			txt = $('#log').val();
+			
+			// Prints Final Lex Success Message
+			$('#log').val(txt + "\nLex Completed With " + lexWarningCount + " WARNING(S) and " + lexErrorCount + " ERROR(S)" + "...");
 
-    return [tokenArray, lexWarningCount, lexErrorCount];
-}
+			//console.log($('#log').val());
 
-function printLastMessage(tokenArray, printTokens, lexWarningCount, lexErrorCount) {
-    // Decides what to print for Final Lex Message
-    if (printTokens) {
-        txt = $('#log').val();
+			$textarea.scrollTop($textarea[0].scrollHeight);
+
+			marqueeTokens = [];
+			tableTokens = [];
+
+			var tokenNumber = 0;
+
+			// Prepares token for printing into marquee and table
+			for (var m = 0; m < tokenArray.length; m++) {
+				marqueeTokens[m] = "<span class=\"tokenStream\">" + "<span class=\"tokenStreamNum\">" + tokenNumber + "</span> " + ":: " + "<span class=\"tokenStreamText\">" + tokenArray[m].type + " [ " + tokenArray[m].value + " ] " + "</span></span>";
+				tableTokens[m] = "<tr class=\"tokenRow\"><td>" + tokenNumber + "</td><td>" + tokenArray[m].type + "</td><td>" + tokenArray[m].value + "</td><td>" + tokenArray[m].line + "</td></tr>";
+				tokenNumber++;
+			}
+			
+			// Updates Progess Status Bar
+			if(lexWarningCount == 0)
+				$('#lexResults').html("<span style=\"color:green;\"> PASSED </span>");
+			else
+				$('#lexResults').html("<span style=\"color:#d58512;\"> PASSED </span>");
+
+			// Prints token into marquee and table
+			document.getElementById('marquee-holder').innerHTML = "<marquee id='token-banner' behavior='scroll' direction='left' onmouseover='this.stop();' onmouseout='this.start();'>" + marqueeTokens.join("") + "</marquee>";
+			document.getElementById('tokenTable').innerHTML = "<th>Token Number</th><th>Token Type</th><th>Value</th><th>Line Number</th>" + tableTokens.join("");
+		} 
+		else {
+			txt = $('#log').val();
+			
+			// Prints Final Lex Fail Message
+			$('#log').val(txt + "\nLex Failed With " + lexWarningCount + " WARNING(S) and " + lexErrorCount + " ERROR(S)" + "...");
+			
+			$textarea.scrollTop($textarea[0].scrollHeight);
+			
+			// Updates Progess Status Bar
+			$('#lexResults').html("<span style=\"color:red;\"> FAILED </span>");
+		}
 		
-		// Prints Final Lex Success Message
-        $('#log').val(txt + "\nLex Completed With " + lexWarningCount + " WARNING(S) and " + lexErrorCount + " ERROR(S)" + "...");
+		printLastMessage.tokenArray = tokenArray;
+		printLastMessage.lexWarningCount = lexWarningCount;
+		printLastMessage.lexErrorCount = lexErrorCount;
 
-        //console.log($('#log').val());
-
-        $textarea.scrollTop($textarea[0].scrollHeight);
-
-        marqueeTokens = [];
-        tableTokens = [];
-
-        var tokenNumber = 0;
-
-        // Prepares token for printing into marquee and table
-        for (var m = 0; m < tokenArray.length; m++) {
-            marqueeTokens[m] = "<span class=\"tokenStream\">" + "<span class=\"tokenStreamNum\">" + tokenNumber + "</span> " + ":: " + "<span class=\"tokenStreamText\">" + tokenArray[m].type + " [ " + tokenArray[m].value + " ] " + "</span></span>";
-            tableTokens[m] = "<tr class=\"tokenRow\"><td>" + tokenNumber + "</td><td>" + tokenArray[m].type + "</td><td>" + tokenArray[m].value + "</td><td>" + tokenArray[m].line + "</td></tr>";
-            tokenNumber++;
-        }
-		
-		// Updates Progess Status Bar
-		if(lexWarningCount == 0)
-			$('#lexResults').html("<span style=\"color:green;\"> PASSED </span>");
-		else
-			$('#lexResults').html("<span style=\"color:#d58512;\"> PASSED </span>");
-
-        // Prints token into marquee and table
-        document.getElementById('marquee-holder').innerHTML = "<marquee id='token-banner' behavior='scroll' direction='left' onmouseover='this.stop();' onmouseout='this.start();'>" + marqueeTokens.join("") + "</marquee>";
-        document.getElementById('tokenTable').innerHTML = "<th>Token Number</th><th>Token Type</th><th>Value</th><th>Line Number</th>" + tableTokens.join("");
-    } 
-	else {
-        txt = $('#log').val();
-		
-		// Prints Final Lex Fail Message
-        $('#log').val(txt + "\nLex Failed With " + lexWarningCount + " WARNING(S) and " + lexErrorCount + " ERROR(S)" + "...");
-		
-		$textarea.scrollTop($textarea[0].scrollHeight);
-		
-		// Updates Progess Status Bar
-		$('#lexResults').html("<span style=\"color:red;\"> FAILED </span>");
-    }
-
-    return [tokenArray, lexWarningCount, lexErrorCount];
+		return printLastMessage;
+	}
 }
--- Projects/Compiler & Dashboard/dist/js/main-page-functions.js
+++ Projects/Compiler & Dashboard/dist/js/main-page-functions.js
@@ -1,9 +1,81 @@
+// Global Boolean Declarations
+var verbose = true;
+
+var byChar = true;
+
+var $textarea = $('#log');
+
+// Function to do a "replace all" on a string. Because this doesn't exit natively in JavaScript
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.split(search).join(replacement);
+};
+
+// Function to do a "replace all" on a string using RegEx. Because this doesn't exit natively in JavaScript
+String.prototype.replaceAllRX = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+// Function to do delete a value from an array. Because apparently this doesn't exit natively in JavaScript
+Array.prototype.clean = function(deleteValue) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == deleteValue) {
+            this.splice(i, 1);
+            i--;
+        }
+    }
+    return this;
+};
+
+// Function that returns a boolean if there is a regex match, used during lex
+function isMatch(rx, str) {
+    if (str.match(rx))
+        return true;
+    else
+        return false;
+}
+
+// Toggles whether the compiler will be in "Verbose Mode" or not. (Might move to different file later)
+function toggleVerbose() {
+    verbose = !verbose;
+
+    if (!verbose) {
+        $('#verboseToggle').html("Verbose: Off")
+        $('#verboseToggle').toggleClass('btn btn-danger btn-lg');
+        $('#verboseToggle').toggleClass('btn btn-default btn-lg');
+		$('#verboseDropDown').toggleClass('btn btn-danger btn-lg dropdown-toggle');
+        $('#verboseDropDown').toggleClass('btn btn-default btn-lg dropdown-toggle');
+    } else {
+        $('#verboseToggle').html("Verbose: On")
+        $('#verboseToggle').toggleClass('btn btn-default btn-lg');
+        $('#verboseToggle').toggleClass('btn btn-danger btn-lg');
+        $('#verboseDropDown').toggleClass('btn btn-default btn-lg dropdown-toggle');
+		$('#verboseDropDown').toggleClass('btn btn-danger btn-lg dropdown-toggle');
+		
+    }
+}
+
+// Toggles whether the Lexer will treat strings as String Blocks or CharLists
+function toggleByChar() {
+	byChar = !byChar;
+	
+	if (!byChar) {
+        $('#byCharToggle').html("Lex String By: Char")
+    } else {
+        $('#byCharToggle').html("Lex String By: Block")
+    }
+}
+
+// Copies all log messages to clipboard
 function cloneLog() {
 	$('#log').select();
     document.execCommand('copy');
 	clearSelection();
 	showToast('copyMessage');
 }
+
+// Clears selection of text in log after cloneLog is called
 function clearSelection() {
     if ( document.selection ) {
         document.selection.empty();
@@ -11,11 +83,15 @@ function clearSelection() {
         window.getSelection().removeAllRanges();
     }
 }
+
+// Shows custom toast messages, as a form of notification for the user
 function showToast(toastId) {
     var x = document.getElementById(toastId);
     x.className = "toast show";
     setTimeout(function(){ x.className = x.className.replace("toast show", "toast"); }, 1500);
 }
+
+// Changes Smiley's teeth according to Compulation Stage.
 function brokenTeeth(brokenLevel) {
 	if (brokenLevel == 0)
 		$('#teeth').attr('src', '../img/teeth.png');
