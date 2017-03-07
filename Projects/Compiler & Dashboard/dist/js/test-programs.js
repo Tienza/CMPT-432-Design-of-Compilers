110227fb5218b7ff62b916b3f84127492aff463d
Tienza <Piradon.Liengtiraphan1@gmail.com>
2017-03-06 21:45:59 -0500

    Added parse fail test programs

--- Projects/Compiler & Dashboard/dist/js/test-programs.js
+++ Projects/Compiler & Dashboard/dist/js/test-programs.js
@@ -4,29 +4,39 @@ function insertProgram(progNum) {
 	var testProgramArray;
 	var testProgram = "";
 	
-	// Clears Console and Inserts Test Program 0
+	// Inserts Test Program 0
 	if (progNum == 0)
 		testProgramArray = [ "{", "}", "$"];
-	// Clears Console and Inserts Test Program 1
+	// Inserts Test Program 1
 	else if (progNum == 1)
 		testProgramArray = [ "{", "\n", "\t", "p", "r", "i", "n", "t", "(", "\"", "t", "h", "e", "r", "e", " ", "i", "s", " ", "n", "o", " ", "s", "p", "o", "o", "n", "\"", ")", "\n", "}", "$" ];
-	// Clears Console and Inserts Alan's Test Case From Project 1
+	// Inserts Alan's Test Case From Project 1
 	else if (progNum == 2)
 		testProgramArray = [ "{", "}", "$", "\t", "\n", "{", "{", "{", "{", "{", "{", "}", "}", "}", "}", "}", "}", "$", "\t", "\n", "{", "{", "{", "{", "{", "{", "}", "}", "}", "}", "}", "}", "}", "$", "\t", "\n", "{", "i", "n", "t", "\t", "@", "}", "$" ];
-	// Clears Console and Inserts Large Test Case
+	// Inserts Large Test Case
 	else if (progNum == 3)
 		testProgramArray = [ "{", "\n", "\t", "i", "n", "t", " ", "a", "\n", " ", " ", " ", " ", "\t", "i", "n", "t", " ", "b", "\n", "\n", " ", " ", " ", " ", "\t", "a", " ", "=", " ", "0", "\n", " ", " ", " ", " ", "\t", "b", " ", "=", " ", "0", "\n", "\n", " ", " ", " ", " ", "\t", "w", "h", "i", "l", "e", " ", "(", "a", " ", "!", "=", " ", "3", ")", " ", "{", "\n", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "p", "r", "i", "n", "t", "(", "a", ")", "\n", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "w", "h", "i", "l", "e", " ", "(", "b", " ", "!", "=", " ", "3", ")", " ", "{", "\n", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "\t", "p", "r", "i", "n", "t", "(", "b", ")", "\n", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "\t", "b", " ", "=", " ", "1", " ", "+", " ", "b", "\n", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "\t", "i", "f", " ", "(", "b", " ", "=", "=", " ", "2", ")", " ", "{", "\n", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "\t", "p", "r", "i", "n", "t", "(", "\"", "t", "h", "e", "r", "e", " ", "i", "s", " ", "n", "o", " ", "s", "p", "o", "o", "n", "\"", ")", "\n", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "\t", "}", "\n", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "}", "\n", "\n", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "b", " ", "=", " ", "0", "\n", " ", " ", " ", " ", " ", " ", " ", " ", "\t", "a", " ", "=", " ", "1", " ", "+", " ", "a", "\n", " ", " ", " ", "\t", " ", "}", "\n", "}" , "$" ];
-	// Clears Console and Inserts Test Case With Missing $ at the end
+	// Inserts Test Case With Missing $ at the end
 	else if (progNum == 4)
 		testProgramArray = [ "{", "\n", "\t", "i", "n", "t", " ", "a", "\n", "\t", "a", " ", "=", " ", "1", "\n", "}" ];
-	// Clears Console and Inserts If Statement Test Case
+	// Inserts If Statement Test Case
 	else if (progNum == 5)
 		testProgramArray = [ "{", "\n", "\t", "i", "n", "t", " ", "a", "\n", "\t", "a", " ", "=", " ", "1", "\n", "\n", "\t", "i", "f", "(", "\"", "a", "\"", " ", "=", "=", " ", "\"", "a", "\"", ")", " ", "{", "\n", "\t", "\t", "a", " ", "=", " ", "2", "\n", "\t", "}", "\n", "\n", "\t", "i", "f", "(", "a", " ", "!", "=", " ", "1", ")", " ", "{", "\n", "\t", "\t", "a", " ", "=", " ", "3", "\n", "\t", "}", "\n", "\n", "\t", "i", "f", "(", "a", " ", "=", "=", " ", "1", ")", " ", "{", "\n", "\t", "\t", "a", " ", "=", " ", "3", "\n", "\t", "}", "\n", "}", " ", "$" ]
-	// Clears Console and Inserts Invalid Print Statement Case
+	// Inserts Invalid Print Statement Case
 	else if (progNum == 6)
 		testProgramArray = [ "{", "\n", "\t", "p", "r", "i", "n", "t", "(", "\"", "1", "2", "\"", ")", "\n", "}", "$" ];
+	// Inserts Line Break String Case
 	else if (progNum == 7)
 		testProgramArray = [ "{", "\"", "t", "w", "o", "\n", "l", "i", "n", "e", "s", "\"", "}", "$" ];
+	// Inserts Parse Fail Case 0
+	else if (progNum == 8)
+		testProgramArray = [ "{", "\n", "\t", "s", "t", "r", "i", "n", "g", " ", "1", "\n", "}", "$" ];
+	// Inserts Parse Fail Case 1
+	else if (progNum == 9)
+		testProgramArray = [ "{", "\n", "\t", "a", " ", "+", " ", "3", "\n", "}", "$" ];
+	// Inserts Parse Fail Case 2
+	else if (progNum == 10)
+		testProgramArray = [ "{", "\n", "\t", "p", "r", "i", "n", "t", "(", "$", ")", "\n", "}", "$" ];
 	// If no valid program number is provided, print WARNING to log
 	else {
 		availableProgram = false;
