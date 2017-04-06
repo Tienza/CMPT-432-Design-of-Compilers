3e852586f965338a13f69001065867e4d12add95
Piradon Liengtiraphan <Piradon.Liengtiraphan1@gmail.com>
2017-04-06 12:22:36 -0400

    Code now traverses tree properly

--- Projects/Compiler & Dashboard/dist/js/make-symbol-table.js
+++ Projects/Compiler & Dashboard/dist/js/make-symbol-table.js
@@ -1,6 +1,28 @@
 function makeSymbolTable() {
 	var makeASTReturns = makeAST();
 
+	var ast = makeASTReturns.AST;
+
 	if (verbose)
 		console.log(makeASTReturns);
+
+	console.log(ast.cur);
+	traverseTree(ast.cur);
+
+	function traverseTree(node) {
+		if (node.children.length != 0) {
+			var printed = 0;
+			node.children.forEach(function(element){
+				if (printed == 0) {		
+					console.log(node.name);
+					printed = 1;
+				}
+				traverseTree(element);
+			});
+			
+		}
+		else {
+			console.log("Reached End of Branch... " + node.name + " on line " + node.line);
+		}
+	}
 }
