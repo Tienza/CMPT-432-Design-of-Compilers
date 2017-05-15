216ce52d0c42e27108d4768560e77c8c260a4462
Piradon Liengtiraphan <Piradon.Liengtiraphan1@gmail.com>
2017-05-15 19:08:10 -0400

    Aesthetic Changes

--- Projects/Compiler & Dashboard/dist/js/compile.js
+++ Projects/Compiler & Dashboard/dist/js/compile.js
@@ -13,16 +13,22 @@ function compile() {
 	var totalWarningCount = codeGenerationReturns.totalWarningCount;
 	var totalErrorCount =  codeGenerationReturns.totalErrorCount;
 
-	var machineCode = ["A9","AD","8D","6D","A2","AE","A0","AC","EA","EC","D0","EE","FF"]; 
+	var machineCode = ["A9","6D","A2","A0","EA","D0","EE"]; 
+	var memoryMachineCode = ["AD","8D","AE","AC","EC"];
+	var printMachineCode = "FF";
 	var ascii = ["61","62","63","64","65","66","67","68","69","6A","6B","6C","6D","6E","6F","70","71","72","73","74","75","76","77","78","79","7A"];
 
 	for (var i = 0; i < codeTable.length; i++) {
 		if (machineCode.includes(codeTable[i]) && i < dynamicMemStart)
-			codeTable[i] = "<span style=\"color:#d9534f;\">" + codeTable[i] + "</span>";
-		else if (ascii.includes(codeTable[i]) && i > dynamicMemStart)
-			codeTable[i] = "<span style=\"color:#5cb85c;\">" + codeTable[i] + "</span>";
+			codeTable[i] = "<span style=\"color:#990000;\">" + codeTable[i] + "</span>";
+		else if (memoryMachineCode.includes(codeTable[i]) && i < dynamicMemStart)
+			codeTable[i] = "<span style=\"color:#bc7410\">" + codeTable[i] + "</span>";
+		else if (codeTable[i] == printMachineCode && i < dynamicMemStart)
+			codeTable[i] = "<span style=\"color:#000000\">" + codeTable[i] + "</span>";
+		else if (ascii.includes(codeTable[i]) && i >= dynamicMemStart)
+			codeTable[i] = "<span style=\"color:#275927;\">" + codeTable[i] + "</span>";
 		else if (hexTable.includes(codeTable[i]))
-			codeTable[i] = "<span style=\"color:#286090;\">" + codeTable[i] + "</span>";
+			codeTable[i] = "<span style=\"color:#214f78;\">" + codeTable[i] + "</span>";
 	}
 
 	var printCode = "";
